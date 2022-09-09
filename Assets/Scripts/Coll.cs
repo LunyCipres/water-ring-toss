1c043e5d737df0aceabce552aaed22c3ab62c536
Grifind0r <abdullahbinnaeempro@gmail.com>
2022-09-09 12:24:07 +0500

    final 2
    last

--- Assets/Scripts/Coll.cs
+++ Assets/Scripts/Coll.cs
@@ -26,16 +26,22 @@ public class Coll : MonoBehaviour
         float _x = water_peg.transform.position.x;
         float _y = water_peg.transform.position.y;
         float _z = water_peg.transform.position.z;
-
-        if (_x < x)
+        if (y > 4.1f) 
         {
-            gameObject.transform.position = new Vector3((x - attractiveForce), y, z);
+
+            if (_x < x)
+            {
+                gameObject.transform.position = new Vector3((x - attractiveForce), y, z);
+            }
+
+            else if (_x > x)
+            {
+                gameObject.transform.position = new Vector3((x + attractiveForce), y, z);
+            }
+
         }
 
-        else if (_x > x)
-        {
-            gameObject.transform.position = new Vector3((x + attractiveForce), y, z);
-        }
+        
         /* if (_z < z)
          {
              gameObject.transform.position = new Vector3(x , y , (z - 0.001f));
