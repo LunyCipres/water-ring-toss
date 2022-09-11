89bacfb5596eec0120dfc736a207def067a467af
Grifind0r <abdullahbinnaeempro@gmail.com>
2022-09-11 18:31:11 +0500

    Update Score.cs
    made score public so that  bilal can use it in level changing

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -6,7 +6,7 @@ using TMPro;
 
 public class Score : MonoBehaviour
 {
-    private int score = 0;
+    public int score = 0;
     [SerializeField]
     private TMP_Text _title;
     /*private void OnCollisionEnter(Collision collision)
