d2346e17bf9de68227d3e729fc84a444c6d5826d
LunyCipres <sarahnoor12232@gmail.com>
2022-09-16 21:20:25 +0500

    fixing level starting time

--- Assets/Scripts/TimeControl.cs
+++ Assets/Scripts/TimeControl.cs
@@ -7,7 +7,7 @@ public class TimeControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Time.timeScale = 1;   
+        Time.timeScale = 0;
     }
 
     public void FreezeTime() {
