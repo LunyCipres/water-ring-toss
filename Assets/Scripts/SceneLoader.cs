6c8ea79a063138432d7833db237ef63fc52e9fca
LunyCipres <sarahnoor12232@gmail.com>
2022-09-18 19:52:56 +0500

    Added restart button

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -9,4 +9,8 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
